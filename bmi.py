52fc77605436b76ce0ba86242a831c954fc72384
Fawad Khan <imfavadkhan@gmail.com>
2025-08-05 10:12:32 +0500

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,8 @@
+#BMI = weight / (height * height)
+
+weight = float(input("Enter your weight in KG:"))
+height = float(input("Enter your height in meters:"))
+
+BMI = weight / (height * height)
+
+print(f"Your BMI is :{BMI}")
